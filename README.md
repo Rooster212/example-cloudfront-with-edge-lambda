ca8f1db11a71fd32ca139087c897be270fc1ca91
Jamie Roos <jamieroos1@gmail.com>
2023-11-08 19:52:16 +0000

    Initial example

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
-# Welcome to your CDK TypeScript project
+# CDK CloudFront with Lambda@Edge origin request example
 
-This is a blank project for CDK development with TypeScript.
+This example shows how to deploy a CloudFront origin using CDK, with a origin request Lambda. In this example I will show how to request 2 different static sites from one CloudFront distribution and S3 bucket, but this could be used for many other things such as load balanced servers, changing buckets based on region and more.
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+## Usage of OAI vs OAC
+
+Origin Access Identity is a more legacy way to allow Cloudfront to access an S3 bucket, and is discouraged by AWS.
+
+CloudFormation does support OACs:
+https://hayao-k.dev/migrating-cloudfront-oai-to-oac-using-cloudformation
+
+However, CDK does not directly yet as per this GitHub issue:
+https://github.com/aws/aws-cdk/issues/21771
+
+For now I've left it with an OAI until CDK has been updated to work with OACs.
 
 ## Useful commands
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `cdk deploy` deploy this stack to your default AWS account/region
+- `cdk diff` compare deployed stack with current state
+- `cdk synth` emits the synthesized CloudFormation template
--- lib/example-cloudfront-with-edge-lambda-stack.ts
+++ lib/example-cloudfront-with-edge-lambda-stack.ts
@@ -1,16 +1,125 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as targets from "aws-cdk-lib/aws-route53-targets";
+import * as cloudfront_origins from "aws-cdk-lib/aws-cloudfront-origins";
+import { Code, Runtime } from "aws-cdk-lib/aws-lambda";
+import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
 
 export class ExampleCloudfrontWithEdgeLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const edgeFunction = new cloudfront.experimental.EdgeFunction(
+      this,
+      "ExampleEdgeFunction",
+      {
+        code: Code.fromAsset("lambda"),
+        handler: "origin-request.handler",
+        runtime: Runtime.NODEJS_18_X,
+      }
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ExampleCloudfrontWithEdgeLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const bucket = new Bucket(this, "ExampleBucket", {
+      bucketName: `rooster212-example-cloudfront-${this.account}-${this.region}`,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+
+      // Just for testing!
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(
+      this,
+      `ExampleCloudfrontOAI`,
+      {
+        comment: `OAI for Example CDK deployment`,
+      }
+    );
+
+    // Grant access to Cloudfront
+    bucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [bucket.arnForObjects("*")],
+        principals: [
+          new iam.CanonicalUserPrincipal(
+            cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    );
+
+    bucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ["s3:ListBucket"],
+        resources: [bucket.bucketArn],
+        principals: [
+          new iam.CanonicalUserPrincipal(
+            cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    );
+
+    // Create a CloudFront web distribution
+    const distribution = new cdk.aws_cloudfront.Distribution(
+      this,
+      "ExampleDistribution",
+      {
+        defaultRootObject: "index.html",
+        priceClass: cdk.aws_cloudfront.PriceClass.PRICE_CLASS_100,
+        comment: "A test distribution to show how origin lambdas can be used.",
+        errorResponses: [
+          {
+            httpStatus: 403,
+            responseHttpStatus: 403,
+            responsePagePath: "/error.html",
+            ttl: cdk.Duration.minutes(5),
+          },
+        ],
+        defaultBehavior: {
+          origin: new cdk.aws_cloudfront_origins.S3Origin(bucket),
+          viewerProtocolPolicy:
+            cdk.aws_cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+          // Disable caching for this example, but not usually recommended.
+          cachePolicy: cdk.aws_cloudfront.CachePolicy.CACHING_DISABLED,
+          // Specify our edge Lambda here
+          edgeLambdas: [
+            {
+              functionVersion: edgeFunction.currentVersion,
+              eventType: cdk.aws_cloudfront.LambdaEdgeEventType.ORIGIN_REQUEST,
+            },
+          ],
+        },
+      }
+    );
+
+    // Deploy site contents to S3 bucket
+    new s3deploy.BucketDeployment(this, "DeployExampleSite", {
+      sources: [s3deploy.Source.asset("./site")],
+      destinationBucket: bucket,
+      retainOnDelete: false,
+      // Consider changing for production purposes
+      cacheControl: [
+        {
+          value: "max-age=0, no-cache, no-store, must-revalidate",
+        },
+      ],
+      // You can set distribution to cause an invalidation on deploy
+      // This is not required if setting no-cache, which is recommended for testing
+    });
+
+    new cdk.CfnOutput(this, "ExampleBucketOutput", {
+      value: bucket.bucketName,
+    });
+    new cdk.CfnOutput(this, "ExampleDomainNameOutput", {
+      value: "https://" + distribution.domainName,
+    });
   }
 }
